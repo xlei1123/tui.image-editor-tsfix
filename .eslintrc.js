6b06ae89ad4968582c26eb59d219599a8c659841
layxiang <layxiang@tencent.com>
2021-09-09 11:26:11 +0800

    feat: ts类型修复

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['tui/es6', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
+  plugins: ['prettier', 'jest'],
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    es6: true,
+    jest: true,
+    'jest/globals': true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: { array: true, object: true },
+        AssignmentExpression: { array: false, object: false },
+      },
+    ],
+  },
+};
